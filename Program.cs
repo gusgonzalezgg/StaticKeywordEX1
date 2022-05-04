be523f1c8e36cc830d139ea48c4db4501b052fad
Gustavo Gonzalez <gusgonzalez.gg@gmail.com>
2022-05-04 15:35:32 -0400

    commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StaticExercise5422
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! We're going to do a tempurature conversion today");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            Console.WriteLine("Let's start with Farenheit to Celsius");
+            Console.WriteLine();
+            Console.WriteLine("Pick any number");
+            double firstNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"Nice! Let's see what " + firstNumber + " converts to");
+            Console.ReadKey();
+
+            var Celsius = TempConverter.FarenheitToCelsius(firstNumber);
+
+            Console.WriteLine($"Looks like " + firstNumber + " degrees Farenheit converts to " + Celsius + " degrees Celsius!");
+            Console.ReadKey ();
+            
+            Console.WriteLine("Now let's do Celsius to Farenheit");
+            Console.WriteLine();
+            Console.WriteLine("Pick any number");
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"Nice! Let's see how " + secondNumber + " converts");
+            Console.ReadKey();
+
+            var Farenheit = TempConverter.CelsiusToFarenheit(secondNumber);
+
+            Console.WriteLine($"Looks like " + secondNumber + " degrees Celsius converts to " + Farenheit + " degrees Farenheit!");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise5422
+{
+    public static class TempConverter
+    {
+
+        public static double FarenheitToCelsius(double Farenheit) 
+        {
+            double Celsius = (Farenheit - 32) * 5/9 ;
+            return Celsius;
+        }
+
+        public static double CelsiusToFarenheit(double Celsius)
+        { 
+            double Farenheit = (Celsius * 9/5) + 32;
+            return Farenheit;
+        }
+
+    }
+}
